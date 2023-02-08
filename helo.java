6ab1a2ccc9d7f6e10f5b2fd5d8ab38f5b57a2433
vinayaprsad <vinayaprsad035@gmail.com>
2023-02-08 15:39:57 +0530

    first commit

---
+++ helo.java
@@ -0,0 +1,9 @@
+public class helo  {
+    public static void  main(String args[]) {
+        for(int i=0;i<10;i++){
+            System.out.println("helloword");
+        }
+        
+    }
+    
+}
